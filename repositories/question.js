719388e0a9e3a153fc5a1c654447af789c3beb9a
dawid12349 <dawid12349@interia.pl>
2022-10-21 15:19:03 +0200

    implement getAnswer function

--- repositories/question.js
+++ repositories/question.js
@@ -32,7 +32,18 @@ const makeQuestionRepository = fileName => {
 
     return foundQuestion.answers
   }
-  const getAnswer = async (questionId, answerId) => {}
+
+  const getAnswer = async (questionId, answerId) => {
+    const fileContent = await readFile(fileName, { encoding: 'utf-8' })
+    const questions = JSON.parse(fileContent) || []
+    const foundQuestion = questions.find(question => question.id === questionId)
+
+    if (!foundQuestion && !foundQuestion.answers) {
+      return undefined
+    }
+
+    return foundQuestion.answers.find(answer => answer.id === answerId)
+  }
   const addAnswer = async (questionId, answer) => {}
 
   return {
